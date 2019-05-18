eceb763e89ede92a4783687c9920fde94452e0d6
hagiangg <hagiangnguyenphan@gmail.com>
2019-05-18 18:47:51 +0700

    session18

--- session17/js/example1.js
+++ session17/js/example1.js
@@ -1,11 +1,18 @@
 function checkRegister() {
-	var name = document.getElementById('name').value;
-	var phone = document.getElementById('phone').value;
-	var address = document.getElementById('address').value;
-	var firstday = document.getElementById('firstday').value;
-	var lastday = document.getElementById('lastday').value;
-	var firstnumber = document.getElementById('firstnumber').value;
-	var lastnumber = document.getElementById('lastnumber').value;
+//	var name = document.getElementById('name').value;
+//	var phone = document.getElementById('phone').value;
+//	var address = document.getElementById('address').value;
+//	var firstday = document.getElementById('firstday').value;
+//	var lastday = document.getElementById('lastday').value;
+//	var firstnumber = document.getElementById('firstnumber').value;
+//	var lastnumber = document.getElementById('lastnumber').value;
+	var name = $("#name").val();
+	var address = $("#address").val();
+	var phone = $("#phone").val();
+	var firstday = $("#firstday").val();
+	var lastday = $("#lastday").val();
+	var firstnumber = $("#firstnumber").val();
+	var lastnumber = $("#lastnumber").val();
 	var check = true;	
 	if (name == '') {
 		document.getElementById('errorname').innerHTML = 'Nhập tên';
@@ -59,10 +66,14 @@ function checkRegister() {
 }
 
 function calculate() {
-	var firstday = document.getElementById('firstday').value;
-	var lastday = document.getElementById('lastday').value;
-	var firstnumber = document.getElementById('firstnumber').value;
-	var lastnumber = document.getElementById('lastnumber').value;
+//	var firstday = document.getElementById('firstday').value;
+//	var lastday = document.getElementById('lastday').value;
+//	var firstnumber = document.getElementById('firstnumber').value;
+//	var lastnumber = document.getElementById('lastnumber').value;
+	var firstday = $("#firstday").val();
+	var lastday = $("#lastday").val();
+	var firstnumber = $("#firstnumber").val();
+	var lastnumber = $("#lastnumber").val();
 	if (firstday >= lastday) document.getElementById('errorfirst').innerHTML = 'Nhập ngày đầu kỳ nhỏ hơn cuối kỳ';
 	if (firstnumber >= lastnumber) document.getElementById('errorlastnumber').innerHTML = 'Nhập số đầu kỳ nhỏ hơn số cuối kỳ';
 	else {
@@ -73,6 +84,10 @@ function calculate() {
 		if (sum > 100 && sum < 300) sum = 100 * 1500 + (sum - 100) * 2000;
 		else sum = 100 * 1500 + 200 * 2000 + (sum - 200) * 3000;
 	}
-	document.getElementById('print').innerHTML = '<br>Số tiền điện: ' + sum;
+	document.getElementById('print').innerHTML = '<br>Số tiền điện: ' + format_currency(sum);
 }
-
+function format_currency(a) {
+ 	a = a.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+	return a;
+}
+// document.write(format_currency(10000));
